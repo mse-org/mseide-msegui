28441c0612e550fc4bcb137f4736e87c2b7b8e58
mse1 <mse1@56ecf5fe-6917-0410-8ac6-ca8e178bccbf>
2006-11-07 06:33:56 +0000

    + mseconsts_es.
    git-svn-id: https://mseide-msegui.svn.sourceforge.net/svnroot/mseide-msegui/trunk@372 56ecf5fe-6917-0410-8ac6-ca8e178bccbf

--- lib/common/kernel/mseconsts_es.pas
+++ lib/common/kernel/mseconsts_es.pas
@@ -6,7 +6,7 @@
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-}
+} 
 unit mseconsts_es;
 {$ifdef FPC}{$mode objfpc}{$h+}{$INTERFACES CORBA}{$endif}
 interface
