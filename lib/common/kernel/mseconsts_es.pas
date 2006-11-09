84c5b5ff27671405484b2499b26198fb371c8175
mse1 <mse1@56ecf5fe-6917-0410-8ac6-ca8e178bccbf>
2006-11-09 07:31:16 +0000

    * Updated const files.
    git-svn-id: https://mseide-msegui.svn.sourceforge.net/svnroot/mseide-msegui/trunk@390 56ecf5fe-6917-0410-8ac6-ca8e178bccbf

--- lib/common/kernel/mseconsts_es.pas
+++ lib/common/kernel/mseconsts_es.pas
@@ -6,6 +6,9 @@
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+    Spanish translation by Julio Jimenez Borreguero.
+    
 } 
 unit mseconsts_es;
 {$ifdef FPC}{$mode objfpc}{$h+}{$INTERFACES CORBA}{$endif}
