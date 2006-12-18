179dea8eb3db0580a5b812c7454e1e861478ab21
mse1 <mse1@56ecf5fe-6917-0410-8ac6-ca8e178bccbf>
2006-12-18 09:34:58 +0000

    * Post 1.0rc2.
    git-svn-id: https://mseide-msegui.svn.sourceforge.net/svnroot/mseide-msegui/branches/fixes_1_0@541 56ecf5fe-6917-0410-8ac6-ca8e178bccbf

--- apps/ide/main.pas
+++ apps/ide/main.pas
@@ -28,7 +28,7 @@ uses
  msegraphutils,typinfo,msedock,sysutils,msesysenv,msestrings,
  msepostscriptprinter;
 const
- versiontext = '1.0rc2';
+ versiontext = '1.0x';
  idecaption = 'MSEide';
 type
  envvarty = (env_vargroup,env_np);
