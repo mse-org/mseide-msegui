c082fcdb3c314a051eff27e2db746be7e24d01ed
mse1 <mse1@56ecf5fe-6917-0410-8ac6-ca8e178bccbf>
2007-01-20 19:07:41 +0000

    * Fixed tcustomcaptionframe.pointincaption..
    git-svn-id: https://mseide-msegui.svn.sourceforge.net/svnroot/mseide-msegui/branches/fixes_1_0@668 56ecf5fe-6917-0410-8ac6-ca8e178bccbf

--- lib/common/kernel/msewidgets.pas
+++ lib/common/kernel/msewidgets.pas
@@ -1,4 +1,4 @@
-{ MSEgui Copyright (c) 1999-2006 by Martin Schreiber
+{ MSEgui Copyright (c) 1999-2007 by Martin Schreiber
 
     See the file COPYING.MSE, included in this distribution,
     for details about the copyright.
@@ -1533,7 +1533,7 @@ begin
   result:= false;
  end
  else begin
-  updatestate;
+  checkstate;
   rect1:= finfo.dest;
   with rect1 do begin
    case fcaptionpos of
