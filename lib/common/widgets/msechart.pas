5a001952543af029023b3f837f1ffbeef9ad89ed
mse1 <mse1@56ecf5fe-6917-0410-8ac6-ca8e178bccbf>
2007-06-01 17:45:36 +0000

    * tdial,tchart.
    git-svn-id: https://mseide-msegui.svn.sourceforge.net/svnroot/mseide-msegui/trunk@1211 56ecf5fe-6917-0410-8ac6-ca8e178bccbf

--- lib/common/widgets/msechart.pas
+++ lib/common/widgets/msechart.pas
@@ -1,3 +1,12 @@
+{ MSEgui Copyright (c) 2007 by Martin Schreiber
+
+    See the file COPYING.MSE, included in this distribution,
+    for details about the copyright.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+}
 unit msechart;
 {$ifdef FPC}{$mode objfpc}{$h+}{$INTERFACES CORBA}{$endif}
 interface
