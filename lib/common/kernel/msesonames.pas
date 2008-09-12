7a1c97d8fba49b728a6190549699082dec26c73e
mse1 <mse1@56ecf5fe-6917-0410-8ac6-ca8e178bccbf>
2008-09-12 13:40:21 +0000

    * msesonames.
    git-svn-id: https://mseide-msegui.svn.sourceforge.net/svnroot/mseide-msegui/trunk@2485 56ecf5fe-6917-0410-8ac6-ca8e178bccbf

--- lib/common/kernel/msesonames.pas
+++ lib/common/kernel/msesonames.pas
@@ -25,14 +25,19 @@ const
                                      ('libfontconfig.so.1','libfontconfig.so');
  xftnames: array[0..1] of filenamety = ('libXft.so.2','libXft.so');
  icenames: array[0..1] of filenamety = ('libICE.so.6','libICE.so');
- smnames: array[0..1] of filenamety = ('libSM.so6','libSM.so');
+ smnames: array[0..1] of filenamety = ('libSM.so.6','libSM.so');
  
  sqlite3lib: array[0..1] of filenamety = ('libsqlite3.so.0','libsqlite3.so'); 
- sslnames: array[0..1] of filenamety = ('libssl.so.0.9.8','libssl.so');
- sslutilnames: array[0..1] of filenamety = ('libcrypto.so.0.9.8','libcrypto.so');  
- fbembedlib: array[0..1] of filenamety = ('libfbembed.so.2','libfbembed.so');
- fbcgdslib: array[0..2] of filenamety = ('libfbclient.so','libfbclient.so',
-                                         'libgds.so');
+ sslnames: array[0..3] of filenamety = (
+           'libssl.so.0.9.8','libssl.so.0.9.7','libssl.so.0.9.6',
+           'libssl.so');
+ sslutilnames: array[0..3] of filenamety = (
+           'libcrypto.so.0.9.8','libcrypto.so.0.9.7','libcrypto.so.0.9.6',
+           'libcrypto.so');  
+ fbembedlib: array[0..2] of filenamety = ('libfbembed.so.2','libfbembed.so.1',
+                                          'libfbembed.so');
+ fbcgdslib: array[0..3] of filenamety = ('libfbclient.so.2','libfbclient.so.1',
+                                          'libfbclient.so','libgds.so');
 {$endif}
 
 implementation
