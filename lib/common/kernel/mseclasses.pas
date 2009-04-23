dbd50fed4f8598435ac2a84a3dbf7d74ceedfdb5
mse1 <mse1@56ecf5fe-6917-0410-8ac6-ca8e178bccbf>
2009-04-23 07:44:00 +0000

    * tmenuitem.getfont, getfontactive, setfont, setfontactive checks fowner for nil. + getoptionalobject(const aowner: tcomponent,...).
    git-svn-id: https://mseide-msegui.svn.sourceforge.net/svnroot/mseide-msegui/branches/fixes_2_0@2952 56ecf5fe-6917-0410-8ac6-ca8e178bccbf

--- lib/common/kernel/mseclasses.pas
+++ lib/common/kernel/mseclasses.pas
@@ -550,10 +550,15 @@ procedure createobjectlinker(const owner: iobjectlink; onevent: objectlinkeventt
  //sets finstancepo
 procedure getoptionalobject(const componentstate: tcomponentstate;
                        const instance: tobject; createproc: createprocty); overload;
+procedure getoptionalobject(const aowner: tcomponent; //can be nil
+                       const instance: tobject; createproc: createprocty); overload;
 procedure getoptionalobject(const componentstate: tcomponentstate;
                   var instance: tvirtualpersistent;
                          const aclass: virtualpersistentclassty); overload;
 procedure setoptionalobject(const componentstate: tcomponentstate;
+                  const value: tpersistent; var instance;
+                    createproc: createprocty); overload;
+procedure setoptionalobject(const aowner: tcomponent; //can be nil
                   const value: tpersistent; var instance;
                     createproc: createprocty); overload;
 procedure setoptionalobject(const componentstate: tcomponentstate;
@@ -1883,6 +1888,18 @@ begin
  end;
 end;
 
+procedure getoptionalobject(const aowner: tcomponent;
+                       const instance: tobject; createproc: createprocty);
+var
+ sta1: tcomponentstate;
+begin
+ sta1:= [];
+ if aowner <> nil then begin
+  sta1:= aowner.componentstate;
+ end;
+ getoptionalobject(sta1,instance,createproc);
+end;
+
 procedure getoptionalobject(const componentstate: tcomponentstate;
            var instance: tvirtualpersistent; const aclass: virtualpersistentclassty);
 begin
@@ -1906,6 +1923,19 @@ begin
  else begin
   freeandnil(tpersistent(instance));
  end;
+end;
+
+procedure setoptionalobject(const aowner: tcomponent; //can be nil
+                  const value: tpersistent; var instance;
+                    createproc: createprocty); overload;
+var
+ sta1: tcomponentstate;
+begin
+ sta1:= [];
+ if aowner <> nil then begin
+  sta1:= aowner.componentstate;
+ end;
+ setoptionalobject(sta1,value,instance,createproc);
 end;
 
 procedure setoptionalobject(const componentstate: tcomponentstate;
