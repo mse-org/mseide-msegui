a66b45c925cbc22aec92bea07c1c5c85505f90d8
mse1 <mse1@56ecf5fe-6917-0410-8ac6-ca8e178bccbf>
2009-08-20 15:00:31 +0000

    * Fixed msedirclean.cmd.
    git-svn-id: https://mseide-msegui.svn.sourceforge.net/svnroot/mseide-msegui/trunk@3160 56ecf5fe-6917-0410-8ac6-ca8e178bccbf

--- tools/msedirclear.cmd
+++ tools/msedirclear.cmd
@@ -2,4 +2,4 @@
 if "%1"=="" @echo A directory must be supplied! & exit -1
 dir /ad %1
 if not %errorlevel% equ 0 (@echo %1 must be an existing directory! & exit -1)
-for /R %%F in ("%1") do cd %%F && del *.o & del *.ppu & del *.a & del & for %%M in (*.mfm) do form2pas %%M 
+for /R %%F in ("%1") do cd %%F && del *.o & del *.ppu & del *.a & for %%M in (*.mfm) do form2pas %%M 
