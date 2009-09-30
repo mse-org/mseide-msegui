258afee267f008020e20612608ad4b470faa2288
mse1 <mse1@56ecf5fe-6917-0410-8ac6-ca8e178bccbf>
2009-09-30 16:03:53 +0000

    + Macro definitions by environment variables.
    git-svn-id: https://mseide-msegui.svn.sourceforge.net/svnroot/mseide-msegui/trunk@3234 56ecf5fe-6917-0410-8ac6-ca8e178bccbf

--- apps/ide/guitemplates.pas
+++ apps/ide/guitemplates.pas
@@ -6,15 +6,33 @@ uses
  msesysenv;
 
 type
- envvarty = (env_macrodef,env_vargroup,env_np,env_ns,env_filename);
+ envvarty = (env_macrodef,env_vargroup,env_np,env_ns,
+             env_fpcdir,env_fpclibdir,env_msedir,env_mselibdir,env_syntaxdefdir,
+             env_templatedir,env_compstoredir,env_compiler,env_debugger,
+             env_exeext,env_target,
+             env_filename);
 const
  sysenvvalues: array[envvarty] of argumentdefty =
   ((kind: ak_pararg; name: '-macrodef'; anames: nil; flags: []),
    (kind: ak_pararg; name: '-macrogroup'; anames: nil; flags: []),
    (kind: ak_par; name: 'np'; anames: nil; flags: []), //no project
    (kind: ak_par; name: 'ns'; anames: nil; flags: []), //no skin
+   (kind: ak_envvar; name: 'FPCDIR'; anames: nil; flags: []),
+   (kind: ak_envvar; name: 'FPCLIBDIR'; anames: nil; flags: []),
+   (kind: ak_envvar; name: 'MSEDIR'; anames: nil; flags: []),
+   (kind: ak_envvar; name: 'MSELIBDIR'; anames: nil; flags: []),
+   (kind: ak_envvar; name: 'SYNTAXDEFDIR'; anames: nil; flags: []),
+   (kind: ak_envvar; name: 'TEMPLATEDIR'; anames: nil; flags: []),
+   (kind: ak_envvar; name: 'COMPSTOREDIR'; anames: nil; flags: []),
+   (kind: ak_envvar; name: 'COMPILER'; anames: nil; flags: []),
+   (kind: ak_envvar; name: 'DEBUGGER'; anames: nil; flags: []),
+   (kind: ak_envvar; name: 'EXEEXT'; anames: nil; flags: []),
+   (kind: ak_envvar; name: 'TARGET'; anames: nil; flags: []),
    (kind: ak_arg; name: ''; anames: nil; flags: []));
-   
+
+ firstenvvarmacro = env_fpcdir;   
+ lastenvvarmacro = env_target;   
+ 
 type
  tguitemplatesmo = class(tmsedatamodule)
    fadevertkonvex: tfacecomp;
@@ -43,6 +61,16 @@ var
 begin
  result:= nil;
  with guitemplatesmo.sysenv do begin
+  for int1:= ord(firstenvvarmacro) to ord(lastenvvarmacro) do begin
+             //envvar macros can be overridden by --macrodef
+   if defined[int1] then begin
+    setlength(result,high(result) + 2);
+    with result[high(result)] do begin
+     name:= sysenvvalues[envvarty(int1)].name;
+     value:= guitemplatesmo.sysenv.value[int1];
+    end;
+   end;
+  end;
   ar1:= values[ord(env_macrodef)];
   for int1:= 0 to high(ar1) do begin
    ar2:= nil;
