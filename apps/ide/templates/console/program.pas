a289a243fd31e9882e88e91b0045db79451ef604
mse1 <mse1@56ecf5fe-6917-0410-8ac6-ca8e178bccbf>
2009-10-09 15:41:56 +0000

    - cthtread removed from console project template.
    git-svn-id: https://mseide-msegui.svn.sourceforge.net/svnroot/mseide-msegui/trunk@3247 56ecf5fe-6917-0410-8ac6-ca8e178bccbf

--- apps/ide/templates/console/program.pas
+++ apps/ide/templates/console/program.pas
@@ -2,7 +2,6 @@ program ${%PROGRAMNAME%};
 {$ifdef FPC}{$mode objfpc}{$h+}{$endif}
 {$ifdef mswindows}{$apptype console}{$endif}
 uses
- {$ifdef FPC}{$ifdef linux}cthreads,{$endif}{$endif}
  sysutils;
 begin
 end.
