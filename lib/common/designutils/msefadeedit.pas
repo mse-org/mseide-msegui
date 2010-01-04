851b860801b1630e81002813aa03825513124e38
mse1 <mse1@56ecf5fe-6917-0410-8ac6-ca8e178bccbf>
2010-01-04 08:38:50 +0000

    * Fixed method signature in tfadeeditfo.
    git-svn-id: https://mseide-msegui.svn.sourceforge.net/svnroot/mseide-msegui/branches/fixes_2_2@3448 56ecf5fe-6917-0410-8ac6-ca8e178bccbf

--- lib/common/designutils/msefadeedit.pas
+++ lib/common/designutils/msefadeedit.pas
@@ -1,3 +1,19 @@
+{ MSEide Copyright (c) 2007-2010 by Martin Schreiber
+   
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+}
 unit msefadeedit;
 {$ifdef FPC}{$mode objfpc}{$h+}{$endif}
 interface
@@ -37,6 +53,7 @@ type
                    const apos: pointty; const offset: pointty;
                    const objects: integerarty);
    procedure endpickev(const sender: tcustompickwidget; const apos: pointty;
+                    const ashiftstate: shiftstatesty;
                    const offset: pointty; const objects: integerarty);
    procedure resizeev(const sender: TObject);
    procedure dataenteterev(const sender: TObject);
@@ -458,8 +475,8 @@ begin
 end;
 
 procedure tfadeeditfo.endpickev(const sender: tcustompickwidget;
-               const apos: pointty; const offset: pointty;
-               const objects: integerarty);
+               const apos: pointty; const ashiftstate: shiftstatesty;
+               const offset: pointty; const objects: integerarty);
 var
  int1: integer;
  rect1: rectty;
