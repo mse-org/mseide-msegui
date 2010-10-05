545c083961ec82a8687a7e5ee54c693faadf4428
mse1 <mse1@56ecf5fe-6917-0410-8ac6-ca8e178bccbf>
2010-10-05 08:56:07 +0000

    * wo_taskbar removed from mainfo. * wo_taskbar removed from defaultmainformoptionswindow.
    git-svn-id: https://mseide-msegui.svn.sourceforge.net/svnroot/mseide-msegui/trunk@3984 56ecf5fe-6917-0410-8ac6-ca8e178bccbf

--- lib/common/widgets/mseforms.pas
+++ lib/common/widgets/mseforms.pas
@@ -1,4 +1,4 @@
-{ MSEgui Copyright (c) 1999-2008 by Martin Schreiber
+{ MSEgui Copyright (c) 1999-2010 by Martin Schreiber
 
     See the file COPYING.MSE, included in this distribution,
     for details about the copyright.
@@ -38,7 +38,7 @@ const
  defaultformoptions = [fo_autoreadstat,fo_autowritestat,
                        fo_savepos,fo_savezorder,fo_savestate];
  defaultmainformoptions = defaultformoptions + [fo_main,fo_terminateonclose];
- defaultmainformoptionswindow = [wo_groupleader,wo_taskbar];
+ defaultmainformoptionswindow = [wo_groupleader{,wo_taskbar}];
  
  defaultformwidgetoptions = (defaultoptionswidgetmousewheel - 
                          [ow_mousefocus{,ow_tabfocus}]) + [ow_subfocus,ow_hinton];
