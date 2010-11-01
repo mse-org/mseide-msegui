aa1c12f147baddb54b310a83c4dd6d57c0904dba
mse1 <mse1@56ecf5fe-6917-0410-8ac6-ca8e178bccbf>
2010-11-01 07:07:30 +0000

    * Pre 2.4.
    git-svn-id: https://mseide-msegui.svn.sourceforge.net/svnroot/mseide-msegui/trunk@4057 56ecf5fe-6917-0410-8ac6-ca8e178bccbf

--- apps/ide/main.pas
+++ apps/ide/main.pas
@@ -32,7 +32,7 @@ uses
  classes,mseclasses,msegraphutils,typinfo,msedock,sysutils,msesysenv,msestrings,
  msepostscriptprinter,msegraphics,mseglob,mseprocmonitorcomp,msesys,mserttistat;
 const
- versiontext = '2.5 unstable';
+ versiontext = '2.4';
 {$ifdef linux}
  {$ifdef CPU64}
   hosttext = 'x86_64-linux';
--- lib/common/kernel/msegui.pas
+++ lib/common/kernel/msegui.pas
@@ -26,7 +26,7 @@ uses
                                {$ifdef mse_with_ifi},mseifiglob{$endif};
 
 const
- mseguiversiontext = '2.5 unstable';
+ mseguiversiontext = '2.4';
  
  defaultwidgetcolor = cl_default;
  defaulttoplevelwidgetcolor = cl_background;
