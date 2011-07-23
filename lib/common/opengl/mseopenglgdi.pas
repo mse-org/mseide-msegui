3724b0efb2e8f9b7045b300a3dfbfca46eb5d274
mse1 <mse1@56ecf5fe-6917-0410-8ac6-ca8e178bccbf>
2011-07-23 08:23:59 +0000

    + tfontcache.
    git-svn-id: https://mseide-msegui.svn.sourceforge.net/svnroot/mseide-msegui/trunk@4676 56ecf5fe-6917-0410-8ac6-ca8e178bccbf

--- lib/common/opengl/mseopenglgdi.pas
+++ lib/common/opengl/mseopenglgdi.pas
@@ -70,45 +70,76 @@ type
  end;
  pgluvertexty = ^gluvertexty;
  ppgluvertexty = ^pgluvertexty;
- 
+{ 
  ftglfontty = record
   handle: pftglfont;
  end;
  pftglfontty = ^ftglfontty;
-
- glfonthdataty = record
+}
+{
+ cachefontty = record
+ end;
+ pcachefontty = ^cachefontty;
+}
+{
+ fontcachehdataty = record
   height: integer;
   name: string;
- end; 
- glfontdataty = record
-  h: glfonthdataty;
-  handle: pftglfont;
+ end;
+}
+ fontcachedataty = record
+//  h: fontcachehdataty;
+  height: integer;
+  name: string;
   refcount: integer;
- end;
- pglfontdataty = ^glfontdataty;
- glfonthashdataty = record
-  header: hashheaderty;
-  data: glfontdataty;
- end;
- pglfonthashdataty = ^glfonthashdataty;
 
- tglfontcache = class(thashdatalist)
+  ascent: integer;
+  descent: integer;
+  linespacing: integer;
+  caretshift: integer;
+  
+  font: ptruint;
+//  handle: pcachefontty;
+ end;
+ pfontcachedataty = ^fontcachedataty;
+ fontcachehashdataty = record
+  header: hashheaderty;
+  data: fontcachedataty;
+ end;
+ pfontcachehashdataty = ^fontcachehashdataty;
+
+ tfontcache = class(thashdatalist)
   protected
    function hashkey(const akey): hashvaluety; override;
    function checkkey(const akey; const aitemdata): boolean; override;
    procedure finalizeitem(var aitemdata); override;
-   function find(const afont: fontdataty): pglfontdataty;
+   function find(const afont: fontdataty): pfontcachedataty;
+   procedure internalfreefont(const afont: ptruint); virtual;
+   function internalgetfont(const ainfo: getfontinfoty): boolean;
+                                                    virtual; abstract;
+   procedure updatefontinfo(var adata: fontcachedataty); virtual; abstract;
+  public
+   constructor create; 
+   procedure getfont(var drawinfo: drawinfoty);
+   procedure freefontdata(var drawinfo: drawinfoty);
+   procedure gettext16width(var drawinfo: drawinfoty); virtual; abstract;
+   procedure getchar16widths(var drawinfo: drawinfoty); virtual; abstract;
+   procedure getfontmetrics(var drawinfo: drawinfoty); virtual; abstract;
+ end;
+
+ tglfontcache = class(tfontcache)
+  protected
+   procedure internalfreefont(const afont: ptruint); override;
+   function internalgetfont(const ainfo: getfontinfoty): boolean; override;
+   procedure updatefontinfo(var adata: fontcachedataty); override;
   public
    constructor create;
    destructor destroy; override;
- 
-   procedure getfont(var drawinfo: drawinfoty);
-   procedure freefontdata(var drawinfo: drawinfoty);
-   procedure gettext16width(var drawinfo: drawinfoty);
-   procedure getchar16widths(var drawinfo: drawinfoty);
-   procedure getfontmetrics(var drawinfo: drawinfoty);
+   procedure gettext16width(var drawinfo: drawinfoty); override;
+   procedure getchar16widths(var drawinfo: drawinfoty); override;
+   procedure getfontmetrics(var drawinfo: drawinfoty); override;
  end;
-  
+   
 var
  ffontcache: tglfontcache;
  gdinumber: integer;
@@ -135,7 +166,8 @@ type
   pd: paintdevicety;
   gclineoptions: lineoptionsty;
   sourceheight: integer;
-  ftglfont: ftglfontty;
+//  ftglfont: ftglfontty;
+  ftglfont: pftglfont;
   tess: pglutesselator;
  end;
 
@@ -443,7 +475,8 @@ begin
    gclineoptions:= lineinfo.options;
   end;
   if gvm_font in mask then begin
-   ftglfont:= pftglfontty(font)^;
+//   ftglfont:= pftglfontty(font)^;
+   ftglfont:= pointer(font);
   end;
   if gvm_clipregion in mask then begin
    if clipregion = 0 then begin
@@ -676,7 +709,7 @@ begin
   gltranslatef(pos^.x,sourceheight-pos^.y,0);
 //  glrasterpos2i(pos^.x,sourceheight-pos^.y);
   glwindowpos2i(pos^.x,sourceheight-pos^.y);
-  ftglrenderfont(ftglfont.handle,pchar(str1),ftgl_render_all);
+  ftglrenderfont(ftglfont,pchar(str1),ftgl_render_all);
   glpopmatrix;
  end;
 end;
@@ -797,58 +830,81 @@ begin
  result:= gdinumber;
 end;
 
-{ tglfontcache }
+{ tfontcache }
 
-constructor tglfontcache.create;
+constructor tfontcache.create;
 begin
- initializeftgl([]);
- inherited create(sizeof(glfontdataty));
+ inherited create(sizeof(fontcachedataty));
  fstate:= fstate + [hls_needsnull,hls_needsfinalize];
 end;
 
-destructor tglfontcache.destroy;
+procedure tfontcache.finalizeitem(var aitemdata);
 begin
- inherited;
- releaseftgl;
+ finalize(fontcachedataty(aitemdata));
 end;
 
-procedure tglfontcache.finalizeitem(var aitemdata);
-begin
- finalize(glfontdataty(aitemdata));
-end;
-
-function tglfontcache.hashkey(const akey): hashvaluety;
+function tfontcache.hashkey(const akey): hashvaluety;
 begin
  with fontdataty(akey) do begin
   result:= stringhash(h.name) xor h.d.height;
  end;
 end;
 
-function tglfontcache.checkkey(const akey; const aitemdata): boolean;
+function tfontcache.checkkey(const akey; const aitemdata): boolean;
 begin
- with fontdataty(akey),glfontdataty(aitemdata).h do begin
+ with fontdataty(akey),fontcachedataty(aitemdata) do begin
   result:= (h.name = name) and (h.d.height = height);
  end;
 end;
 
-function tglfontcache.find(const afont: fontdataty): pglfontdataty;
+function tfontcache.find(const afont: fontdataty): pfontcachedataty;
 begin
  result:= pointer(internalfind(afont));
  if result <> nil then begin
-  result:= @pglfonthashdataty(result)^.data;
+  result:= @pfontcachehashdataty(result)^.data;
  end;
 end;
 
-procedure tglfontcache.getfont(var drawinfo: drawinfoty);
+procedure tfontcache.getfont(var drawinfo: drawinfoty);
 var
- po1: pglfontdataty;
+ po1: pfontcachedataty;
+begin
+ with drawinfo.getfont do begin
+  ok:= true;
+  po1:= find(fontdata^);
+  with fontdata^ do begin 
+   font:= 0;
+   if po1 = nil then begin
+    if not internalgetfont(drawinfo.getfont) then begin
+     ok:= false;
+     exit;
+    end;
+    po1:= @((internaladd(fontdata^)^.data));
+    po1^.name:= fontdata^.h.name;
+    po1^.height:= fontdata^.h.d.height;
+    po1^.font:= font;
+    updatefontinfo(po1^);
+   end;
+   inc(po1^.refcount);
+   ascent:= po1^.ascent;
+   descent:= po1^.descent;
+   linespacing:= po1^.linespacing;
+   caretshift:= po1^.caretshift;
+  end;
+ end;
+end;
+
+(*
+procedure tfontcache.getfont(var drawinfo: drawinfoty);
+var
+ po1: pfontcachedataty;
  po2: pftglfont;
 begin
  with drawinfo.getfont do begin
   ok:= true;
   po1:= find(fontdata^);
   with fontdata^ do begin 
-   getmem(pointer(font),sizeof(ftglfontty));
+   getmem(pointer(font),ffontdatasize);
    with pftglfontty(font)^ do begin
     if po1 = nil then begin
      po2:= ftglcreatepixmapfont('/usr/share/fonts/truetype/arial.ttf');
@@ -861,7 +917,7 @@ begin
      po1:= @((internaladd(fontdata^)^.data));
      po1^.h.name:= fontdata^.h.name;
      po1^.h.height:= fontdata^.h.d.height;
-     po1^.handle:= po2;
+     po1^.handle:= pointer(po2);
      ftglsetfontcharmap(po1^.handle,ft_encoding_unicode);
      ftglsetfontfacesize(po1^.handle,20,72);
     end;
@@ -875,22 +931,24 @@ begin
   end;
  end;
 end;
+*)
+procedure tfontcache.internalfreefont(const afont: ptruint);
+begin
+ //dummy
+end;
 
-procedure tglfontcache.freefontdata(var drawinfo: drawinfoty);
+procedure tfontcache.freefontdata(var drawinfo: drawinfoty);
 var
- po1: pglfontdataty;
+ po1: pfontcachedataty;
 begin
  with drawinfo.getfont do begin
   po1:= find(fontdata^);
   dec(po1^.refcount);
   with fontdata^ do begin 
    if po1^.refcount = 0 then begin
-    with pftglfontty(font)^ do begin
-     ftgldestroyfont(handle);
-    end;
+    internalfreefont(font);
    end;
    internaldeleteitem(phashdatadataty(po1));
-   freemem(pointer(font));
   end;
  end;
  if count = 0 then begin
@@ -898,35 +956,76 @@ begin
  end;
 end;
 
+{ tglfontcache }
+
+constructor tglfontcache.create;
+begin
+ initializeftgl([]);
+ inherited create;
+end;
+
+destructor tglfontcache.destroy;
+begin
+ inherited;
+ releaseftgl;
+end;
+
+procedure tglfontcache.internalfreefont(const afont: ptruint);
+begin
+ ftgldestroyfont(pointer(afont));
+end;
+
+function tglfontcache.internalgetfont(const ainfo: getfontinfoty): boolean;
+var
+ po1: pftglfont;
+begin
+ result:= false;
+ po1:= ftglcreatepixmapfont('/usr/share/fonts/truetype/arial.ttf');
+ if po1 <> nil then begin
+  ftglsetfontcharmap(po1,ft_encoding_unicode);
+  ftglsetfontfacesize(po1,20,72);
+  ainfo.fontdata^.font:= ptruint(po1);
+  result:= true;
+ end;
+end;
+
+
+procedure tglfontcache.updatefontinfo(var adata: fontcachedataty);
+begin
+ with adata do begin
+  ascent:= round(ftglgetfontascender(pointer(font)));
+  descent:= -round(ftglgetfontdescender(pointer(font)));
+  linespacing:= round(ftglgetfontlineheight(pointer(font)));
+  caretshift:= 0;
+ end;
+end;
+
 procedure tglfontcache.gettext16width(var drawinfo: drawinfoty);
 begin
  with drawinfo.gettext16width do begin
-  with pftglfontty(fontdata^.font)^ do begin
-   result:= round(ftglgetfontadvance(handle,pchar(stringtoutf8(text))));
-  end;
+  result:= round(ftglgetfontadvance(pointer(fontdata^.font),
+                                            pchar(stringtoutf8(text))));
  end;
 end;
 
 procedure tglfontcache.getchar16widths(var drawinfo: drawinfoty);
 var
  f1: cfloat;
- int1{,int2}: integer;
+ int1: integer;
  po1: pmsechar;
  po2: pinteger;
 begin
  with drawinfo.getchar16widths do begin
-  with pftglfontty(fontdata^.font)^ do begin
-   f1:= 0;
-//   int2:= 0;
-   po1:= text;
-   po2:= resultpo;
-   for int1:= count-1 downto 0 do begin
-    f1:= f1 + ftglgetfontadvance(handle,pchar(stringtoutf8(po1^)));
-    po2^:= round(f1);
-    f1:= f1-po2^;
-    inc(po1);
-    inc(po2);
-   end;
+  f1:= 0;
+  po1:= text;
+  po2:= resultpo;
+  for int1:= count-1 downto 0 do begin
+   f1:= f1 + ftglgetfontadvance(pointer(fontdata^.font),
+                                  pchar(stringtoutf8(po1^)));
+   po2^:= round(f1);
+   f1:= f1-po2^;
+   inc(po1);
+   inc(po2);
   end;
  end;
 end;
@@ -937,14 +1036,12 @@ var
  str1: string;
 begin
  with drawinfo.getfontmetrics do begin
-  with pftglfontty(fontdata^.font)^ do begin
-   str1:= stringtoutf8(char);
-   with resultpo^ do begin
-    width:= round(ftglgetfontadvance(handle,pchar(str1)));
-    ftglgetfontbbox(handle,pchar(str1),length(str1),bbox);
-    leftbearing:= round(bbox.left);
-    rightbearing:= width-round(bbox.right); //correct???
-   end;
+  str1:= stringtoutf8(char);
+  with resultpo^ do begin
+   width:= round(ftglgetfontadvance(pointer(fontdata^.font),pchar(str1)));
+   ftglgetfontbbox(pointer(fontdata^.font),pchar(str1),length(str1),bbox);
+   leftbearing:= round(bbox.left);
+   rightbearing:= width-round(bbox.right); //correct???
   end;
  end;
 end;
