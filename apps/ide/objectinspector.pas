67184d7ccef015f6203f96be2b0bff25c65ad11a
Martin Schreiber <mse00000@gmail.com>
2011-11-14 10:33:04 +0100

    * Fixed isrowexit() with noexeitgrid parameter. + tcustomgrid.cellexiting(). * Fixed show mainvalue for focused multiselect property editor.

--- apps/ide/objectinspector.pas
+++ apps/ide/objectinspector.pas
@@ -249,7 +249,7 @@ type
   protected
    feditor: tpropertyeditor;
   public
-   procedure updatestate;
+   procedure updatestate(const exitrow: boolean = false);
  end;
  ppropertyvalue = ^tpropertyvalue;
 
@@ -496,10 +496,12 @@ end;
 
 { tpropertyvalue }
 
-procedure tpropertyvalue.updatestate;
+procedure tpropertyvalue.updatestate(const exitrow: boolean = false);
 begin
  with feditor do begin
-  if allequal then begin
+  if allequal or 
+            (not exitrow and (objectinspectorfo.values.item = self) and 
+                (objectinspectorfo.grid.col = 1)) then begin
    caption:= removelinebreaks(getvalue);
   end
   else begin
@@ -628,7 +630,9 @@ begin
    tpropertyitem(props.itemlist[int1]).updatestate;
    tpropertyvalue(values.itemlist[int1]).feditor:=
            tpropertyitem(props.itemlist[int1]).feditor;
-   tpropertyvalue(values.itemlist[int1]).updatestate;
+   if not grid.cellexiting then begin
+    tpropertyvalue(values.itemlist[int1]).updatestate;
+   end;
   end;
  finally
   props.itemlist.endupdate;
@@ -1295,6 +1299,11 @@ begin
    end;
   finally
    values.itemlist.decupdate;
+  end;
+ end
+ else begin
+  if isrowexit(info,true) and (info.cellbefore.row < grid.rowcount) then begin
+   tpropertyvalue(values[info.cellbefore.row]).updatestate(true);
   end;
  end;
 end;
