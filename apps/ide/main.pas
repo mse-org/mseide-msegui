c164016e927ab4519d087b18ce89804ff287f235
Martin Schreiber <mse00000@gmail.com>
2011-12-14 17:45:28 +0100

    * Fixed version labels.

--- apps/ide/main.pas
+++ apps/ide/main.pas
@@ -34,7 +34,7 @@ uses
  mserttistat,
  msedatanodes,mseedit,mseifiglob,mselistbrowser,projecttreeform;
 const
- versiontext = '2.7 unstable';
+ versiontext = '2.8rc1';
 {$ifdef linux}
  {$ifdef CPU64}
   hosttext = 'x86_64-linux';
