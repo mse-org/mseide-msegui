821079885eea310cc7a6a2b9206c444952754630
Martin Schreiber <mse00000@gmail.com>
2012-01-10 13:16:30 +0100

    * Fixed tcustomdataedit.internalgreidtovalue() out -> var parameter.

--- lib/common/editwidgets/msedataedits.pas
+++ lib/common/editwidgets/msedataedits.pas
@@ -108,7 +108,7 @@ type
    function checkgriddata: tdatalist; overload;
    function checkgriddata(var index: integer): tdatalist; overload; 
                         //index -1 -> grid.row, nil if no focused row
-   procedure internalgetgridvalue(index: integer; out value);
+   procedure internalgetgridvalue(index: integer; var value);
    procedure internalsetgridvalue(index: integer; const Value);
    procedure internalfillcol(const value);
    procedure internalassigncol(const value);
@@ -2120,7 +2120,7 @@ begin
  end;
 end;
 
-procedure tcustomdataedit.internalgetgridvalue(index: integer; out value);
+procedure tcustomdataedit.internalgetgridvalue(index: integer; var value);
 begin
  checkgrid;
  fgridintf.getdata(index,value);
