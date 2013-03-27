04e781b5e2e411518bc4ed9d172f503d3c866302
Martin Schreiber <mse00000@gmail.com>
2013-03-27 07:46:25 +0100

    * mclasses in msenogui.

--- lib/common/kernel/msenogui.pas
+++ lib/common/kernel/msenogui.pas
@@ -1,4 +1,4 @@
-{ MSEgui Copyright (c) 1999-2010 by Martin Schreiber
+{ MSEgui Copyright (c) 1999-2013 by Martin Schreiber
 
     See the file COPYING.MSE, included in this distribution,
     for details about the copyright.
@@ -11,7 +11,7 @@ unit msenogui;
 {$ifdef FPC}{$mode objfpc}{$h+}{$endif}
 interface
 uses
- sysutils,classes,mseapplication,mseevent,msesystypes,msestrings;
+ sysutils,classes,mclasses,mseapplication,mseevent,msesystypes,msestrings;
 type
  tnoguiapplication = class(tcustomapplication)
   private
