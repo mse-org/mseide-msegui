b8bfd4746b0ca68a0bc16773bfb9fd1cb86c0f5a
Martin Schreiber <mse00000@gmail.com>
2013-10-28 07:41:25 +0100

    + mselfsr.pas, 32 bit linear feedback shift register pseudo random generator.

---
+++ lib/common/math/mselfsr.pas
@@ -0,0 +1,45 @@
+{ MSEgui Copyright (c) 2013 by Martin Schreiber
+
+    See the file COPYING.MSE, included in this distribution,
+    for details about the copyright.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+}
+unit mselfsr;
+{$ifdef FPC}{$mode objfpc}{$h+}{$endif}
+interface
+uses
+ msetypes;
+
+//32 bit lfsr x^32+x^30+x^26+x^25+1 
+
+function lfsr32(var sr: uint32): uint32;
+procedure lfsr321(var sr: uint32);
+
+implementation
+const
+ feedbacktable: array[0..255] of byte = (
+  0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,
+  1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,
+  0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,
+  1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,
+  1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,
+  0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,
+  1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,
+  0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0
+ );
+ 
+function lfsr32(var sr: uint32): uint32;
+begin
+ sr:= feedbacktable[sr shr 24] + (sr shl 1);
+ result:= sr;
+end;
+
+procedure lfsr321(var sr: uint32);
+begin
+ sr:= feedbacktable[sr shr 24] + (sr shl 1);
+end;
+
+end.
