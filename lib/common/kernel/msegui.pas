964f5bd4b71ed4dc387f63769ca2df46ae3a2c03
Martin Schreiber <mse00000@gmail.com>
2014-02-04 09:44:04 +0100

    * Post 3.2.

--- lib/common/kernel/msegui.pas
+++ lib/common/kernel/msegui.pas
@@ -26,7 +26,7 @@ uses
                                {$ifdef mse_with_ifi},mseifiglob{$endif};
 
 const
- mseguiversiontext = '3.2';
+ mseguiversiontext = '3.3';
  
  defaultwidgetcolor = cl_default;
  defaulttoplevelwidgetcolor = cl_background;
