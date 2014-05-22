2e83bfa9e6277969ad0a617ed6fde9bece6d20be
Martin Schreiber <mse00000@gmail.com>
2014-05-22 11:04:21 +0200

    + mseiircoeffeditor.

---
+++ lib/common/designutils/mseiircoeffeditor.pas
@@ -0,0 +1,15 @@
+unit mseiircoeffeditor;
+{$ifdef FPC}{$mode objfpc}{$h+}{$endif}
+interface
+uses
+ msetypes,mseglob,mseguiglob,mseguiintf,mseapplication,msestat,msemenus,msegui,
+ msegraphics,msegraphutils,mseevent,mseclasses,msewidgets,mseforms;
+type
+ tmseiircoeffeditorfo = class(tmseform)
+ end;
+var
+ mseiircoeffeditorfo: tmseiircoeffeditorfo;
+implementation
+uses
+ mseiircoeffeditor_mfm;
+end.
