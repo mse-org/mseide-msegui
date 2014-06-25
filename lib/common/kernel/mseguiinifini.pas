d4e4f1dc303e687866d138fb03451662db6ecdd0
Martin Schreiber <mse00000@gmail.com>
2014-06-25 07:20:29 +0200

    + mseguiinifini.pas. * Fixed reading of fpimages with monochrome mask.

---
+++ lib/common/kernel/mseguiinifini.pas
@@ -0,0 +1,19 @@
+{ MSEgui Copyright (c) 2014 by Martin Schreiber
+
+    See the file COPYING.MSE, included in this distribution,
+    for details about the copyright.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+}
+unit mseguiinifini;
+{$ifdef FPC}{$mode objfpc}{$h+}{$endif}
+interface
+implementation
+uses
+ msegui,mseapplication;
+initialization
+ application.initialize();
+finalization
+end.
