62c08f36c81ba65a97e3a004a1fe499bd50f9388
Martin Schreiber <mse00000@gmail.com>
2014-08-20 08:19:35 +0200

    + Release 3.4a.

--- apps/ide/main.pas
+++ apps/ide/main.pas
@@ -38,7 +38,7 @@ uses
  msesystypes,mserttistat,msedatanodes,mseedit,mseifiglob,mselistbrowser,
  projecttreeform,msepipestream,msestringcontainer,msesys;
 const
- versiontext = '3.5';
+ versiontext = '3.4a';
  idecaption = 'MSEide';
 
 type
