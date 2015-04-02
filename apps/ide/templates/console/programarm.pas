3f37d4947bebc6110579a72edf59df41080387e5
Martin Schreiber <mse00000@gmail.com>
2015-04-02 10:09:19 +0200

    + crossarmconsole project template.

---
+++ apps/ide/templates/console/programarm.pas
@@ -0,0 +1,9 @@
+program ${%PROGRAMNAME%};
+{$ifdef FPC}{$mode objfpc}{$h+}{$endif}
+{$ifdef mswindows}{$apptype console}{$endif}
+uses
+ {$ifdef FPC}{$ifdef unix}cthreads,cwstring,{$endif}{$endif}
+ sysutils,xlib; 
+           //xlib necessary in order to avoid linking error, probably FPC bug
+begin
+end.
