6acd0a1b1d4c80fe6fa0ec6bdc4b843d4b14ecd8
Martin Schreiber <mse00000@gmail.com>
2015-05-13 21:46:39 +0200

    * makdist.

--- tools/dist/runmake.sh
+++ tools/dist/runmake.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 MSERUN=/home/mse/packs/standard/git/mseuniverse/tools/mserun/mserun
 FPCINSTDIR=/home/mse/packs/standard/svn/fp/install/
-COMPILERBIN=ppc386
+FPCUNITDIR=lib/fpc/2.6.5/units/x86_64-linux
+COMPILERBIN=lib/fpc/2.6.5/ppcx64
+TARGET=linux
 MSEBASEDIR=/home/mse/packs/standard/git/
-$MSERUN --macrodef=FPCINSTDIR,$FPCINSTDIR --macrodef=COMPILERBIN,$COMPILERBIN --macrodef=MSEBASEDIR,$MSEBASEDIR
+OPT="-X- -Xs"
+echo $OPT
+$MSERUN --macrodef=FPCINSTDIR,$FPCINSTDIR --macrodef=COMPILERBIN,$COMPILERBIN --macrodef=MSEBASEDIR,$MSEBASEDIR --macrodef=FPCUNITDIR,$FPCUNITDIR --macrodef=TARGET,$TARGET "--macrodef=OPT,$OPT" makdist.mrp
