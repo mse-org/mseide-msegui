94274d49cc64e0197bd1fa5b9d2d54004031fc3a
Martin Schreiber <mse00000@gmail.com>
2015-09-17 13:33:44 +0200

    * Explicitely set start value to nil before calling allocuninitedarray().

--- lib/common/widgets/msegrids.pas
+++ lib/common/widgets/msegrids.pas
@@ -4211,8 +4211,7 @@ begin
   autocellsize.cx:= acolwidth;
   autocellsize.cy:= fcellinfo.grid.datarowheight;
   canvas:= fcellinfo.grid.getcanvas;  
-  
-  (acount,sizeof(rows[0]),rows);
+  allocuninitedarray(acount,sizeof(rows[0]),rows);
   int2:= acol;
   startrow:= acol;
   endrow:= acol + acount - 1;
