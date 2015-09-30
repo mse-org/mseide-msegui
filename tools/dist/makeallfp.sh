f007cb054bed3ec2e9199045b5f743c2df3c5224
Martin Schreiber <mse00000@gmail.com>
2015-09-30 12:06:54 +0100

    * Windows dist script.

--- tools/dist/makeallfp.sh
+++ tools/dist/makeallfp.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 BASEDIR=/home/mse/packs/standard/svn/fp/
-FPCDIR=/fixes_2_6
-INSTALLDIR=$BASEDIR/install
+FPCDIR=/fixes_3_0
+INSTALLDIR=$BASEDIR/builds/fixes_3_0
 PWDBEFORE=$PWD
 cd  $BASEDIR/$FPCDIR
 make clean all install INSTALL_PREFIX=$INSTALLDIR
