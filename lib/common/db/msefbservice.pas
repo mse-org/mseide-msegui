1e4060e3c99045bd80348919891a763b4bb45919
Martin Schreiber <mse00000@gmail.com>
2015-12-15 13:47:47 +0100

    + msefbservice.pas. * updated ibase60.inc.

---
+++ lib/common/db/msefbservice.pas
@@ -0,0 +1,12 @@
+unit msefbservice;
+{$ifdef FPC}{$mode objfpc}{$h+}{$endif}
+{under construction}
+interface
+uses
+ mseclasses;
+type
+ tfbservice = class(tmsecomponent)
+ end;
+ 
+implementation
+end.
