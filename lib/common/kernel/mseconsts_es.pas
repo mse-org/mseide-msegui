10dd3bf8af143e439511edf6de6369383f7d4dde
Martin Schreiber <mse00000@gmail.com>
2016-01-13 12:42:15 +0100

    * mseconsts_es.pas.

--- lib/common/kernel/mseconsts_es.pas
+++ lib/common/kernel/mseconsts_es.pas
@@ -157,16 +157,16 @@ const
   'un elemento solamente',             //sc_Single_item_only 
   'Copiar Celdas',                     //sc_Copy_Cells
   'Pegar Celdas',                      //sc_Paste_Cells
-  'Close',               //sc_close
-  'Maximize',            //sc_maximize
-  'Normalize',           //sc_normalize
-  'Minimize',            //sc_minimize
-  'Fix size',            //sc_fix_size
-  'Float',               //sc_float
-  'Stay on top',         //sc_stay_on_top
-  'Stay in background',  //sc_stay_in_background
-  'Lock children',       //sc_lock_children
-  'No lock'              //sc_no_lock
+  'Cerrar',               //sc_close
+  'Maximizar',            //sc_maximize
+  'Restaurar',            //sc_normalize
+  'Minimizar',            //sc_minimize
+  'Ajustar tama'#241'o',  //sc_fix_size
+  'Flotar',               //sc_float
+  'Permanecer en el primer plano',     //sc_stay_on_top
+  'Permanecer en el fondo',            //sc_stay_in_background
+  'Bloquear hijas',                    //sc_lock_children
+  'Sin bloquear'                       //sc_no_lock
 );
     
 function delete_n_selected_rows(const params: array of const): msestring;
