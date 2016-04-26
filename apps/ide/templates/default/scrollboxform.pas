4a122994f46dab27e3b914db730ebe9416904326
Martin Schreiber <mse00000@gmail.com>
2016-04-26 10:02:28 +0200

    + tscrollboxform template.

---
+++ apps/ide/templates/default/scrollboxform.pas
@@ -0,0 +1,15 @@
+unit ${%UNITNAME%};
+{$ifdef FPC}{$mode objfpc}{$h+}{$endif}
+interface
+uses
+ msetypes,mseglob,mseguiglob,mseguiintf,mseapplication,msestat,msemenus,msegui,
+ msegraphics,msegraphutils,mseevent,mseclasses,msewidgets,mseforms;
+type
+ t${%FORMNAME%} = class(tscrollboxform)
+ end;
+var
+ ${%FORMNAME%}: t${%FORMNAME%};
+implementation
+uses
+ ${%UNITNAME%}_mfm;
+end.
