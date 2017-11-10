320ad26aee32c4cb593b11250df7c24810c62abe
Martin Schreiber <mse00000@gmail.com>
2017-11-10 14:36:46 +0100

    + tcustomdatabutton.objectchanged() calls checktemplate of valuefaces.

--- lib/common/editwidgets/msegraphedits.pas
+++ lib/common/editwidgets/msegraphedits.pas
@@ -932,6 +932,7 @@ type
    procedure gridtovalue(arow: integer); override;
    
    procedure setcolor(const avalue: colorty); override;
+   procedure objectchanged(const sender: tobject); override;
 
     //iactionlink
    function getactioninfopo: pactioninfoty;
@@ -4472,6 +4473,16 @@ begin
   inherited;      //no actionchanged
  end;
  setactioncolor(iactionlink(self),avalue);
+end;
+
+procedure tcustomdatabutton.objectchanged(const sender: tobject);
+var
+ i1: int32;
+begin
+ inherited;
+ for i1:= 0 to high(fvaluefaces.fitems) do begin
+  tcustomface(fvaluefaces.fitems[i1]).checktemplate(sender);
+ end;
 end;
 
 procedure tcustomdatabutton.doupdate;
