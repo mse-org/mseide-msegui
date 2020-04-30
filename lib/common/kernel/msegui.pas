b7c4581cba5c145c0df7734a7978a7d22829bbc2
fredvs <fiens@hotmail.com>
2020-04-30 15:37:09 +0200

    Version 5.2.1.

--- lib/common/kernel/msegui.pas
+++ lib/common/kernel/msegui.pas
@@ -34,7 +34,7 @@ uses
                    {$ifdef mse_with_ifi},mseifiglob,mseificompglob{$endif};
 
 const
- mseguiversiontext = '5.1.0';
+ mseguiversiontext = '5.2.1';
  copyrighttext = 'Copyright 1999-2020';
 
  defaultwidgetcolor = cl_default;
