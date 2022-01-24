9133e9000b2ca0ffa09f00f632199ab4e6342881
fredvs <fiens@hotmail.com>
2022-01-24 01:53:11 +0100

    Thanks to Sieghard, updated mseconsts_de.pas

--- lib/common/kernel/mseconsts_de.pas
+++ lib/common/kernel/mseconsts_de.pas
@@ -63,16 +63,16 @@ const
 
  de_stockcaption: stockcaptionaty = (
   '',                        //sc_none
-  'ist ung'#0252'ltig',       //sc_is_invalid
-  'Format Fehler',           //sc_Format_error
+  'ist ung'#0252'ltig',      //sc_is_invalid
+  'Formatfehler',            //sc_Format_error
   'Wert wird ben'#0246'tigt', //sc_Value_is_required
   'Fehler',                  //sc_Error
   'Min',                     //sc_Min
   'Max',                     //sc_Max
-  'Bereichs Fehler',         //sc_Range_error
+  'Bereichsfehler',          //sc_Range_error
 
   '&R'#0252'ckg'#0228'ngig', //sc_Undohk       ///         ///
-  '&Redo',                   //sc_Redohk        //          //
+  '&Wiederholen',            //sc_Redohk        //          //
   '&Kopieren',               //sc_Copyhk        // hotkeys  //
   '&Ausschneiden',           //sc_Cuthk         //          //
   '&Einf'#0252'gen',         //sc_Pastehk       //          // hotkeys
@@ -81,42 +81,42 @@ const
   'Zeile a&nf'#0252'gen',    //sc_append_rowhk  // hotkeys  //
   'Zeile &l'#0246'schen',    //sc_delete_rowhk ///         ///
 
-  '&Dir',                 //sc_Dirhk               ///
+  '&Verzeichnis',         //sc_Dirhk               ///
   '&Home',                //sc_homehk               //
   '&Auf',                 //sc_Uphk                 //
-  'Dir &neu',             //sc_New_dirhk            // hotkeys
+  '&neues Verzeichnis ',  //sc_New_dirhk            // hotkeys
   'N&ame',                //sc_Namehk               //
   '&Verst.Dat.anzeigen',  //sc_Show_hidden_fileshk  //
   '&Filter',              //sc_Filterhk            ///
   'Speichern',            //sc_save
-  #0214'ffnen',            //sc_open
+  #0214'ffnen',           //sc_open
   'Name',                 //sc_name
   'Verzeichnis erstellen',//sc_create_new_directory
-  'Zurück',               //sc_back
-  'Forw'#0228'rts',        //sc_forward
-  'Aufw'#0228'rts',        //sc_up
+  'Zur'#0252'ck',         //sc_back
+  'Vorw'#0228'rts',       //sc_forward
+  'Aufw'#0228'rts',       //sc_up
   'Datei',                //sc_file
   'existiert, wollen Sie '#0252'berschreiben?', //sc_exists_overwrite
-  'wurde geändert, wollen Sie speichern?',     //sc_is_modified_save
+  'wurde ge'#0228'ndert, wollen Sie speichern?',     //sc_is_modified_save
   'WARNUNG',                                   //sc_warningupper
   'FEHLER',                                    //sc_errorupper
   'Exception',                                 //sc_exception
   'System',                                    //sc_system
   'existiert nicht',                           //sc_does_not_exist
-  'PASSWORT',                 //sc_passwordupper
-  'Ppassworteingabe',         //sc_enterpassword
+  'PASSWORT',             //sc_passwordupper
+  'Passworteingabe',      //sc_enterpassword
   'Ung'#0252'ltiges Passwort!',//sc_invalidpassword
   'Verzeichnis kann nicht gelesen werden',     //sc_can_not_read_directory
-  'Grafik Format nicht unterst'#0252'tzt', //sc_graphic_not_supported
-  'Grafik Format Fehler', //sc_graphic_format_error
-  'MS Bitmap',            //sc_MS_Icon
-  'MS Icon',              //sc_MS_Icon
-  'JPEG Bild',            //sc_JPEG_Image
-  'PNG Bild',             //sc_PNG_Image
-  'XPM Bild',             //sc_XPM_Image
-  'PNM Bild',             //sc_PNM_Image
-  'TARGA Bild',           //sc_TARGA_image
-  'TIFF Bild',            //sc_TIFF_image
+  'Grafikformat nicht unterst'#0252'tzt', //sc_graphic_not_supported
+  'Grafikformatfehler',   //sc_graphic_format_error
+  'MS-Bitmap',            //sc_MS_Icon
+  'MS-Icon',              //sc_MS_Icon
+  'JPEG-Bild',            //sc_JPEG_Image
+  'PNG-Bild',             //sc_PNG_Image
+  'XPM-Bild',             //sc_XPM_Image
+  'PNM-Bild',             //sc_PNM_Image
+  'TARGA-Bild',           //sc_TARGA_image
+  'TIFF-Bild',            //sc_TIFF_image
   'Alle',                 //sc_All
   'Best'#0228'tigung',     //sc_Confirmation
   'Datensatz l'#0246'schen?', //sc_Delete_record_question
@@ -133,8 +133,8 @@ const
   'Verwerfen',               //sc_cancel
   'Auffrischen',             //sc_refresh
   'Filter bearbeiten',        //sc_filter_filter
-  'Filter Minimum bearbeiten',//sc_edit_filter_min
-  'Filter Maximum bearbeiten',//sc_filter_edit_max
+  'Filter-Minimum bearbeiten',//sc_edit_filter_min
+  'Filter-Maximum bearbeiten',//sc_filter_edit_max
   'Filter r'#0252'ckstellen',  //sc_reset_filter
   'Filter ein',               //sc_filter_on
   'Suchen',                   //sc_search
@@ -164,8 +164,8 @@ const
   'Normalisieren',             //sc_normalize
   'Minimieren',                //sc_minimize
   'Feste Gr'#0246'sse',        //sc_fix_size
-  'Lösen',                     //sc_float
-  'Im Fordergrund',            //sc_stay_on_top
+  'L'#0246'sen',               //sc_float
+  'Im Vordergrund',            //sc_stay_on_top
   'Im Hintergrund',            //sc_stay_in_background
   'Blockiere Unterfenster',    //sc_lock_children
   'Kein Blockieren',           //sc_no_lock
@@ -177,8 +177,8 @@ const
   'Oberer Rand',               //sc_topborder
   'Rechter Rand',              //sc_rightborder
   'Unterer Rand',              //sc_bottomborder
-  'Text start',                //sc_beginoftext
-  'Text ende',                 //sc_endoftext
+  'Textanfang',                //sc_beginoftext
+  'Textende',                  //sc_endoftext
   'Eingabe Modus',             //sc_inputmode
   #$00DC'berschreiben',        //sc_overwrite
   'Gel'#$00F6'scht',           //sc_deleted
@@ -188,8 +188,8 @@ const
   'R'#$00FC'ckg'#$00E4'ngig',  //sc_withdrawn
   'Fenster aktiviert',         //sc_windowactivated
   'Men'#$00FC,                 //sc_menu
-  'Datei Beginn',              //sc_bof
-  'Datei Ende',                //sc_eof
+  'Dateianfang',               //sc_bof
+  'Dateiende',                 //sc_eof
   'Sprachausgabe',             //sc_voiceoutput
   'Wiederhole Sprachausgabe',  //sc_speakagain
   'Erste Spalte',              //sc_firstcol
