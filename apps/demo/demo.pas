9e9e2977b4576844112e106c8b37fbf0a06b74b0
fredvs <fiens@hotmail.com>
2023-07-04 19:22:06 +0200

    Revert to previous apps/demo/

--- apps/demo/demo.pas
+++ apps/demo/demo.pas
@@ -1,38 +1,8 @@
 program demo;
-uses
-  {$ifdef unix}cthreads, {$endif} sysutils, classes, mseapplication, msegui,mseforms,main;
-
-
-
 {$ifdef FPC}{$mode objfpc}{$h+}{$endif}
- Type
-    TMyThread = class(TThread)
-    protected
-      procedure Execute; override;
-    public
-      Constructor Create(CreateSuspended : boolean);
-    end;
-
- constructor TMyThread.Create(CreateSuspended : boolean);
-  begin
-    inherited Create(CreateSuspended);
-    FreeOnTerminate := True;
-  end;
-
-  
- procedure TMyThread.Execute;
-    begin
-    application.createform(tmainfo,mainfo);
-    application.run;
-    SetCurrentDir('/home/fred/');
-    end;
- 
-
- var
-    MyThread : TMyThread;
-
+uses
+ {$ifdef FPC}{$ifdef unix}cthreads,{$endif}{$endif}msegui,mseforms,main;
 begin
-  MyThread := TMyThread.Create(true); // This way it doesn't start automatically
-  MyThread.Start;
-   sleep(10000);
+ application.createform(tmainfo,mainfo);
+ application.run;
 end.
