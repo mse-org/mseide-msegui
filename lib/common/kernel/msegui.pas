2ebc556dde586f72d40a2c89f6e46fcbcfe0d822
fredvs <fiens@hotmail.com>
2024-03-21 14:02:12 +0100

    MSEgui version 5.10.2.

--- lib/common/kernel/msegui.pas
+++ lib/common/kernel/msegui.pas
@@ -38,7 +38,7 @@ uses
  msebitmap,msearrayprops,msethreadcomp,mserichstring,msearrayutils
                    {$ifdef mse_with_ifi},mseifiglob,mseificompglob{$endif};
  const
- mseguiversiontext = '5.10.0';
+ mseguiversiontext = '5.10.2';
  copyrighttext = 'Copyright 1999-2024';
  defaultwidgetcolor = cl_default;
  defaulttoplevelwidgetcolor = cl_background;
