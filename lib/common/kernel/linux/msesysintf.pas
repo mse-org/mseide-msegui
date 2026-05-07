99ad0c62c4a0bc436b83d5a872dcaaad55a43b38
fredvs <fiens@hotmail.com>
2026-05-08 01:12:59 +0200

    Fixes for freebsd.

--- lib/common/kernel/linux/msesysintf.pas
+++ lib/common/kernel/linux/msesysintf.pas
@@ -28,7 +28,7 @@ interface
 
 uses
 baseunix,
- msesys,msesystypes,msesetlocale,{$ifdef FPC}cthreads, {$if defined(bsd) or defined(darwin)} cwstring {$else} msecwstring {$endif},{$endif}msetypes,
+ msesys,msesystypes,msesetlocale,{$ifdef FPC}cthreads, {$if defined(bsd) or defined(darwin) or defined(freebsd)} cwstring {$else} msecwstring {$endif},{$endif}msetypes,
  mselibc,msectypes,
  msestrings,msestream,classes;
  
